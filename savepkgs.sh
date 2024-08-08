0d5477538530ef16e5dff05c823b461538854ed5
uiriansan <santoswillc@gmail.com>
2024-08-08 02:03:37 -0300

    Save Windows Terminal settings

--- savepkgs.sh
+++ savepkgs.sh
@@ -1,2 +1,5 @@
-
+# Save packages list
 pacman -Qqe > pkglist.txt
+
+# Copy Windows Terminal settings.json
+sudo cp /mnt/c/Users/uiria/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/* ./windows_terminal/
